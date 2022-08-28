27696a42c6fce568e4f3a4e7e6b60f391f337e37
chaerim <whcofla1234@gmail.com>
2022-08-28 17:24:16 +0900

    hobby

--- src/App.js
+++ src/App.js
@@ -137,6 +137,25 @@ function App() {
   )
 }
 
+function Promise() {
+  return (
+    <div id='promise'>
+      <h1>Movie Review</h1>
+      <div className='movie-container'>
+        <h2>제목</h2>
+        <div>
+          내용
+        </div>
+      </div>
+      <div className='form-wrapper'>
+        <input className="title-input" type='text' placeholder='제목' />
+        <textarea className="text-area" placeholder='내용'></textarea>
+      </div>
+      <button className="submit-button">입력</button>
+    </div>
+  );
+}
+
 const styles = {
   input: {
     width: 200,
